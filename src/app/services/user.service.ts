44338c7e427319daf7fa2cd743357ca3ae41a1ec
anthony0tran <anthony0tran@gmail.com>
2020-01-01 17:17:51 +0100

    added free cors proxy. This will prevent the browser from blocking the http request

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,6 +10,8 @@ export class UserService {
   constructor(private httpclient: HttpClient) { }
 
   getUser(username: string): Observable<any> {
-    return this.httpclient.get('https://data.typeracer.com/users?id=tr:' + username + '&universe=play');
+    // This is an open-source CORS proxy necessary to prevent the browser from blocking the http request. (CORS policy warning)
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    return this.httpclient.get(proxy + 'https://data.typeracer.com/users?id=tr:' + username + '&universe=play');
   }
 }
