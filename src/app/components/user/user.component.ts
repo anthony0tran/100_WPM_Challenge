36d822a106021ac88a15cfd37f975db7f0fc8163
anthony0tran <anthony0tran@gmail.com>
2020-01-05 22:32:44 +0100

    Added zoeko5 to the userlist

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -22,7 +22,7 @@ export class UserComponent implements OnInit {
               private dialog: MatDialog) { }
 
   // TODO: Get usernames from database instead of hardcoded.
-  userNames: string[] = ['4n2h0ny', 'yung_typo'];
+  userNames: string[] = ['4n2h0ny', 'yung_typo', 'zoeko5'];
   usersList: User[] = [];
   recentScoresChart: Chart = [];
 
