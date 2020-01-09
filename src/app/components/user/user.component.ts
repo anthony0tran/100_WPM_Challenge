bd70faf7c069a6e486228806573d7fe9b7c1aaa6
anthony0tran <anthony0tran@gmail.com>
2020-01-09 01:36:56 +0100

    added kutspatiebalk

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -22,7 +22,7 @@ export class UserComponent implements OnInit {
               private dialog: MatDialog) { }
 
   // TODO: Get usernames from database instead of hardcoded.
-  userNames: string[] = ['4n2h0ny', 'yung_typo', 'zoeko5', 'min_hd', 'shekster420'];
+  userNames: string[] = ['4n2h0ny', 'yung_typo', 'zoeko5', 'min_hd', 'shekster420', 'kutspatiebalk'];
   usersList: User[] = [];
   recentScoresChart: Chart = [];
 
